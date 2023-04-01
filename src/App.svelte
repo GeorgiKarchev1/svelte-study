a38762a564d9f03f586b919c002df2b86c0ffe73
GeorgiKarchev1 <goshoo429@gmail.com>
2023-04-01 19:34:14 +0300

    My first svelte code

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+</script>
+<main>
+	<nav>
+		<ul>
+		  <li><a href="/">Home</a></li>
+		  <li><a href="/">About</a></li>
+		  <li><a href="/">Contact</a></li>
+		</ul>
+	  </nav>
+</main>
+	  <style>
+		nav {
+		  background-color: #333;
+		  color: #fff;
+		  display: flex;
+		  justify-content: center;
+		  padding: 20px;
+		  width: 100vw;
+		  left: 0;
+		  top: 0;
+		  position: fixed;
+		}
+	  
+		ul {
+		  display: flex;
+		  list-style: none;
+		  margin: 0;
+		  padding: 0;
+		}
+	  
+		li {
+		  margin: 0 10px;
+		}
+	  
+		a {
+		  color: #fff;
+		  text-decoration: none;
+		}
+	  </style>
+	  
